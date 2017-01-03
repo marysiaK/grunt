b6a25ccd41d235469badb7d7830567c4c47fc9a2
marysiaK <marysia.kolaczynska@gmail.com>
2017-01-03 20:32:55 +0100

    grunt zadanie

---
+++ Gruntfile.js
@@ -0,0 +1,51 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    sass: {
+        options: {
+            sourceMap: true
+        },
+        dist: {
+            files: {
+                'css/main.css': 'sass/main.sass'
+            }
+        }
+    },
+
+    imagemin: {
+        dynamic: {
+            files: [{
+                expand: true,
+                cwd: 'images_orig/',
+                src: ['**/*.{png,jpg,jpeg,gif}'],
+                dest: 'images/'
+            }]
+        }
+    },
+
+
+
+    watch: {
+        scripts: {
+            files: ['sass/*.sass'],
+            tasks: ['sass'],
+            options: {
+                spawn: false,
+            },
+        }
+    },
+
+
+  });
+
+  //Load the plugins tasks
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+
+  //Default task(s).
+  grunt.registerTask('default', ['sass', 'imagemin', 'watch']);
+
+};
